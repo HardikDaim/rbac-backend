2675338260ae5c89ca1fc55c26ccd654c32c7423
Hardik Daim <hardikdaim@Hardiks-MacBook-Air.local>
2024-11-22 18:08:08 +0530

    enable cors for frontend

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
 
 // Routes
-const userRoutes = require('./routes/userRoutes');
-const roleRoutes = require('./routes/roleRoutes');
+const userRoutes = require("./routes/userRoutes");
+const roleRoutes = require("./routes/roleRoutes");
 
 dotenv.config();
 const app = express();
 
+const allowedOrigins =
+  process.env.NODE_ENV === "production"
+    ? [process.env.PRODUCTION_URL] // Production URL
+    : [process.env.LOCAL_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // Allow requests with no origin (like mobile apps or Postman)
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,15 +36,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 connectDB();
 
 // API Routes
-app.use('/api/users', userRoutes);
-app.use('/api/roles', roleRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/roles", roleRoutes);
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
-    console.error(err.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+  console.error(err.message);
+  res.status(500).json({ error: "Internal Server Error" });
 });
 
 // Start Server
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
